b91cd0c666e281c7c1c8e3fee51e1e12dc167caf
Loganberbs <58014408+Loganberbs@users.noreply.github.com>
2020-10-01 21:16:48 -0500

    bonus missions 1 and 2

--- Program.cs
+++ Program.cs
@@ -1,35 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace class2_studio
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            Console.WriteLine("Enter a string to be counted: ");
+            
+            string user = Console.ReadLine();
 
+            //string testString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
 
-                char[] ipsumArray = loremIpsum.ToCharArray();
-
-            Dictionary<char, int> counts = new Dictionary<char, int>();
-                foreach(char c in ipsumArray){
-                
-                if (counts.ContainsKey(c)){
-                
-                    int i = counts.GetValueOrDefault(c);
-                    counts[c] = i + 1;
-                } 
-                  else {
-                    counts[c] = 1;
+            foreach (char c in user.ToLower().ToCharArray())
+            {
+                if (!charCount.ContainsKey(c))
+                {
+                    charCount.Add(c, 1);
+                }
+                else
+                {
+                    charCount[c]++;
                 }
             }
-        foreach (KeyValuePair<char, int> count in counts){
-            
+            foreach (KeyValuePair<char, int> count in charCount)
+            {
                 Console.WriteLine(count.Key + ": " + count.Value);
             }
-            Console.ReadLine();
         }
-
     }
 }
